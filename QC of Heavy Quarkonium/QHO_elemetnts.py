Fix units for simulation and save data

QC of Heavy Quarkonium/QHO_elemetnts.py
@@ -3,16 +3,14 @@ import matplotlib.pyplot as plt
 import json
 import scipy.integrate as integrate
 import scipy.special as sp
+from scipy.constants import hbar
 import mpmath
-from qiskit.opflow import X, Y, Z, I
 
-omega = 0.5
-mass = 1.0
-hbar = 1.0
-xZP = hbar / (2*omega*mass)
-kappa = 0.4
-sigma = 200
-mu = 562.9
+omega = 562.9  # MeV
+#  mass = 1.0
+kappa = 0.4063  # MeV
+sigma = 441.6 ** 2  # MeV
+mu = 637.5 # MeV
 b = 1 / np.sqrt(mu * omega)
 
 def kdelta(n, m):

QC of Heavy Quarkonium/VQE 3.py
@@ -17,7 +17,7 @@ from qiskit.quantum_info import SparsePauliOp
 
 
 N = 3
-shots = 2048
+shots = 4 * 2048
 service = QiskitRuntimeService(channel="ibm_quantum", token="0269a959c178542551fbe79731ceecb4685b8fb2c26fbaa03a8450cccef257f33f346fac7f0cca39ede87c0e7cb0312df6cd73f0fc8f4ae9435058af8bffdcd4")
 backend = service.get_backend("ibmq_qasm_simulator")
 
@@ -73,30 +73,26 @@ def build_callback(ansatz, hamiltonian, estimator, callback_dict):
             current_vector (ndarray): Current vector of parameters
                                       returned by optimizer
         """
-        # Keep track of the number of iterations
         callback_dict["iters"] += 1
-        # Set the prev_vector to the latest one
         callback_dict["prev_vector"] = current_vector
-        # Compute the value of the cost function at the current vector
-        callback_dict["cost_history"].append(
-            estimator.run(ansatz, hamiltonian, parameter_values=current_vector)
-            .result()
-            .values[0]
+
+        estimate = estimator.run(ansatz, hamiltonian, parameter_values=current_vector).result()
+        callback_dict["mean_energies"].append(
+            estimate.values[0]
         )
-        # Grab the current time
+        callback_dict["var_energies"].append(
+            estimate.metadata[0]["variance"]
+        )
+
         current_time = time.perf_counter()
-        # Find the total time of the execute (after the 1st iteration)
         if callback_dict["iters"] > 1:
             callback_dict["_total_time"] += current_time - callback_dict["_prev_time"]
-        # Set the previous time to the current time
         callback_dict["_prev_time"] = current_time
-        # Compute the average time per iteration and round it
         time_str = (
             round(callback_dict["_total_time"] / (callback_dict["iters"] - 1), 2)
             if callback_dict["_total_time"]
             else "-"
         )
-        # Print to screen on single line
         print(
             "Iters. done: {} [Avg. time per iter: {}]".format(
                 callback_dict["iters"], time_str
@@ -139,7 +135,8 @@ x0 = np.random.rand(2)
 callback_dict = {
     "prev_vector": None,
     "iters": 0,
-    "cost_history": [],
+    "mean_energies": [],
+    "var_energies": [],
     "_total_time": 0,
     "_prev_time": None,
 }
@@ -156,11 +153,11 @@ with Session(backend=backend):
     )
 
 save_pickle(res)
-save_pickle(callback_dict, filename="callback_dict.json")
+save_pickle(callback_dict, filename="callback_dict.pickle")
 print(res)
 
 fig, ax = plt.subplots()
-ax.plot(range(callback_dict["iters"]), callback_dict["cost_history"])
+ax.errorbar(range(callback_dict["iters"]), callback_dict["mean_energies"], yerr=callback_dict["var_energies"])
 ax.set_xlabel("Iterations")
 ax.set_ylabel("Cost")
 plt.savefig("VQE - quarkonium - test 2")